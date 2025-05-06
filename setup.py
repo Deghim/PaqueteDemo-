7dd5bd077059d18eb34ff2b300360edf01926d20
Jorge <jorgechavira4@gmail.com>
2025-05-06 14:43:25 -0600

    Primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="PaqueteDemo",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Jorge U. Chavira",                         # Tu nombre
+    author_email="jorgechavira4@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/Deghim/PaqueteDemo-",     # URL del proyecto
+)
